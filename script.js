8f58687b3cc3d6f41cf12dfcbbfd4713bb270cb3
Александр <120018968+Djiga13@users.noreply.github.com>
2023-02-09 21:09:37 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let btn = document.querySelector(".button");
+let sun = document.querySelector(".sun");
+let sky = document.querySelector(".sky");
+
+btn.addEventListener("click", function () {
+  if (btn.innerHTML == "включить луну") {
+    sun.style.backgroundColor = "#c5c28d";
+    sky.style.backgroundImage =
+      "url(https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/starfield_stock_1.jpg)";
+    btn.innerHTML = "включить солнце";
+  } else {
+    sky.style.backgroundImage = "linear-gradient(to bottom, #65a9f0, #ebb2b1)";
+    btn.innerHTML = "включить луну";
+    sun.style.backgroundColor = "#f5e555";
+  }
+});
